19796ee3caa7c6baed17104c840626ca1c65cba4
yiyu66 <1226572422@qq.com>
2022-01-10 23:45:16 +0800

    使用interface创建对象类型的接口

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from "react";
+import "./App.css";
+import List from "./components/List";
 function App() {
+  interface IState {
+    people: {
+      name: string;
+      url: string;
+      age: number;
+      note?: string;
+    }[];
+  }
+  const [people, setPeople] = useState<IState["people"]>([]);
+  // const [people, setPeople] = useState([
+  //   {
+  //     name: "梁朝伟",
+  //     url: "",f
+  //     age: 23,
+  //     note: "重庆森林",
+  //   },
+  //   {
+  //     name: "吴彦祖",
+  //     url: "",
+  //     age: 23,
+  //   },
+  // ]);
+
+  people.map((persen) => {
+    console.log(persen.name);
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>人物列表</h1>
+      <List></List>
     </div>
   );
 }
